6181a72e69716444dd6250f9a03b219dcebc693b
Stefan Gavrailov <108361580+Maglata@users.noreply.github.com>
2022-06-29 20:29:37 +0300

    added Improved movement

--- FirstUnityGameCollab/Assets/Scripts/PlayerMovement.cs
+++ FirstUnityGameCollab/Assets/Scripts/PlayerMovement.cs
@@ -4,19 +4,57 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public int MoveSpeed = 2;
+    public float MoveSpeed = 5f;
+    public float Jump = 2f;
+    private bool Canjump;
+    private Rigidbody2D rigidbodycomponent;
+    private bool IsGrounded;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidbodycomponent = GetComponent<Rigidbody2D>(); 
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.W))
+            Canjump = true;
+
+        if (Input.GetKey(KeyCode.D))
+            rigidbodycomponent.velocity = new Vector2(MoveSpeed, rigidbodycomponent.velocity.y);
+
+        if (Input.GetKey(KeyCode.A))
+            rigidbodycomponent.velocity = new Vector2(-MoveSpeed, rigidbodycomponent.velocity.y);
+
+
+    }
+
+
+    // FixedUpdate is called once every physic update
+    private void FixedUpdate()
+    {
+        if (Canjump)
         {
-            transform.Translate(Vector2.up * MoveSpeed * Time.deltaTime);
+            if (!IsGrounded)
+                return;
+
+            rigidbodycomponent.AddForce(Vector2.up * Jump, ForceMode2D.Impulse);
+            Canjump = false;
         }
+            
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        IsGrounded = true;
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        IsGrounded = false;
     }
 }
