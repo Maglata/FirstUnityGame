b03e2ee3a7fdd7a6be3be9e1cf9245654882c3d7
Stefan Gavrailov <108361580+Maglata@users.noreply.github.com>
2022-06-29 14:19:29 +0300

    Added Character Jumping

---
+++ FirstUnityGameCollab/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public int MoveSpeed = 2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            transform.Translate(Vector2.up * MoveSpeed * Time.deltaTime);
+        }
+    }
+}
