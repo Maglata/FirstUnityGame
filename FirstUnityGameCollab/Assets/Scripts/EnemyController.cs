8d2bfd5c3fe4712043da993d4ac62a2fb4259f1a
Stefan Gavrailov <108361580+Maglata@users.noreply.github.com>
2022-07-18 18:31:23 +0300

    Added SFX for Enemy

--- FirstUnityGameCollab/Assets/Scripts/EnemyController.cs
+++ FirstUnityGameCollab/Assets/Scripts/EnemyController.cs
@@ -10,11 +10,14 @@ public class EnemyController : MonoBehaviour
     public int Health = 100;
     int currentHealth;
 
+    private AudioManager_PrototypeHero _audioManager;
+
     // Start is called before the first frame update
     void Start()
     {
         currentHealth = Health;
         _animator = GetComponent<Animator>();
+        _audioManager = AudioManager_PrototypeHero.instance;
     }
 
     // Update is called once per frame
@@ -28,17 +31,19 @@ public class EnemyController : MonoBehaviour
         currentHealth -= damage;
 
         _animator.SetTrigger("Hurt");
+        _audioManager.PlaySound("Hurt");
 
-        if(currentHealth <= 0)
+        if (currentHealth <= 0)
         {
             _animator.SetTrigger("Death");
+            _audioManager.PlaySound("Death");
             DestroyEnemy(); 
         }
     }
 
     private async void DestroyEnemy()
     {
-        await Task.Delay(2000);
+        await Task.Delay(1000);
         Object.Destroy(gameObject);
     }
 }
